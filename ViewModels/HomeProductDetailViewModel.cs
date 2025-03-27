1012f7c8e3ae3826b588a4b29182ebb7de3f8eb7
hungparkour04 <hungparkour04gmail.com>
2025-03-27 21:42:38 +0700

    commit 1

---
+++ ViewModels/HomeProductDetailViewModel.cs
@@ -0,0 +1,10 @@
+﻿using ThuchanhMVC.Models;
+namespace ThuchanhMVC.ViewModels
+{
+    public class HomeProductDetailViewModel
+    {
+        public TDanhMucSp danhMucSp {  get; set; }
+
+        public List<TAnhSp> anhSps { get; set; }
+    }
+}
